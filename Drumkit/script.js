80d8c74c0d54f3efa6e4a024622cd310fb91ca6c
Paaka <Mateuszhartabus@gmail.com>
2019-10-09 23:42:39 +0200

    Added working record and play functions

--- Drumkit/script.js
+++ Drumkit/script.js
@@ -28,85 +28,93 @@ const allSounds = {
         sound:document.querySelector('#tom')
     },
 
-
-
-
-
 }
 const startRecordBtn = document.querySelector('#recordBtn');
+
 const playBtn = document.querySelector('#playBtn');
 const channel1 = [];
+const startingTime = [];
 
 document.body.addEventListener('keypress',playaudio);
+
 playBtn.addEventListener('click', playRecordedAudio)
-
-
 startRecordBtn.addEventListener('click',startTime);
 
 function playaudio(e){
-    const keyPressed = detectKeyAndSound(e);
-    if(keyPressed === 'KeyA'){
-        boomSound.currentTime = 0;
-        boomSound.play();    
-        const time = Date.now()
-        channel1.push({
-            code: e.code,
-            time, // ==> jeśli klucz jest == zmiennej wystarczy podać sam klucz np. time 
-        })
-        console.log(channel1)
-    }
+    const startTime = Date.now();
+    const keyPressed = e.code;
+    console.log(startingTime[0])
+    playSound(keyPressed,allSounds, startingTime[0]);
+   
 }
 
 function startTime(e){
     const timeZero = Date.now();
-    return timeZero
+    startingTime.push(timeZero);
 }
 
-function detectKeyAndSound(e) {
-    const keyPressed = e.code;
-    switch(keyPressed){
-        case "KeyZ":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyX":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyA":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyS":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyD":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyF":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyG":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyC":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
-        case "KeyV":{
-            playSound(keyPressed,allSounds);
-            break;
-        }
+// function detectKeyAndSound(e) {
+//     const keyPressed = e.code;
+//     switch(keyPressed){
+//         case "KeyZ":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyX":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyA":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyS":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyD":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyF":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyG":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyC":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
+//         case "KeyV":{
+//             playSound(keyPressed,allSounds);
+//             break;
+//         }
 
-    }
-}
+//     }
+// }
 
-function playSound(key,list){
+function playSound(key,list,zeroTime){
     list[key].sound.currentTime=0;
     list[key].sound.play();
+    let time = Date.now()
+    console.log(time, zeroTime);
+    const xd = time - zeroTime;
+    channel1.push({
+        code: key,
+        time: xd, // ==> jeśli klucz jest == zmiennej wystarczy podać sam klucz np. time 
+    })
+    console.log(channel1);
+}
 
+function playRecordedAudio(){
+    channel1.forEach((value,i,arr)=>{
+        setTimeout(()=>{
+            const val = value.code
+            allSounds[val].sound.play();// controlse dodać xd
+        },
+        value.time)
+    })
 }

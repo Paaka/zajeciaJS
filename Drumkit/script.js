cdd8af18947de2be6c637b9448b1c782fb73c997
Paaka <Mateuszhartabus@gmail.com>
2019-10-15 15:50:00 +0200

    Now it's working with 4 channals, added key.css

--- Drumkit/script.js
+++ Drumkit/script.js
@@ -29,101 +29,91 @@ const allSounds = {
     },
 
 }
-const startRecordBtn = document.querySelector('#recordBtn');
+const startRecordBtn = document.querySelector('#channel0');
+const startRecordBtn2 = document.querySelector('#channel2');
+const startRecordBtn3 = document.querySelector('#channel3');
+const startRecordBtn4 = document.querySelector('#channel1');
 
 const playBtn = document.querySelector('#playBtn');
-const channel1 = [];
-const startingTime = [];
+
+const startingTime = [ {
+    id:'channel0',
+    time:null,
+    active:false,
+    numInArray:0,
+    song:[]
+},
+{
+    id:'channel1',
+    time:null,
+    active:false,
+    numInArray:1,
+    song:[]
+},
+{
+    id:'channel2',
+    time:null,
+    active:false,
+    numInArray:2,
+    song:[]
+},
+{
+    id:'channel3',
+    time:null,
+    active:false,
+    numInArray:3,
+    song:[]
+},
+];
 
 document.body.addEventListener('keypress',playaudio);
 
+
 playBtn.addEventListener('click', playRecordedAudio)
 startRecordBtn.addEventListener('click',startTime);
+startRecordBtn2.addEventListener('click',startTime);
+startRecordBtn3.addEventListener('click',startTime);
+startRecordBtn4.addEventListener('click',startTime);
 
 function playaudio(e){
     const startTime = Date.now();
     const keyPressed = e.code;
-    console.log(startingTime[0])
-    playSound(keyPressed,allSounds, startingTime[0]);
+    const currentChanel = showActiveChanel();
+
+    playSound(keyPressed,allSounds, currentChanel);
    
 }
 
 function startTime(e){
+    const currentChannel = e.target.id;
+    const indexOfArr = parseInt(currentChannel.slice(7,8));
     const timeZero = Date.now();
-    startingTime.push(timeZero);
-}
+    startingTime[indexOfArr].time = timeZero;
+    startingTime.forEach((el,i,val) => {
+        el.active = false;
+    });
+    startingTime[indexOfArr].active = !startingTime[indexOfArr].active;
+ }
 
-// function detectKeyAndSound(e) {
-//     const keyPressed = e.code;
-//     switch(keyPressed){
-//         case "KeyZ":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyX":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyA":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyS":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyD":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyF":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyG":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyC":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-//         case "KeyV":{
-//             playSound(keyPressed,allSounds);
-//             break;
-//         }
-
-//     }
-// }
-
-function playSound(key,list,zeroTime){
+function playSound(key,list,currentChannal){
     list[key].sound.currentTime=0;
     list[key].sound.play();
-    let time = Date.now()
-    console.log(time, zeroTime);
-    const startTime = time - zeroTime;
+    let time = Date.now();
+    const startTime = time - currentChannal[0].time;
+    
+
     if(!isNaN(startTime)){
-        channel1.push({
+        startingTime[currentChannal[0].numInArray].song.push({
             code: key,
             time: startTime, // ==> jeśli klucz jest == zmiennej wystarczy podać sam klucz np. time 
         })
     }
-    
-  
-    console.log(channel1);
 }
-
-function calculateTime(){
-    channel1.map((val, i, arr) =>{
-        console.log(val.time);
-    })
-}
-
 
 function playRecordedAudio(){
-    const xd = calculateTime();
-    console.log(xd);
-    channel1.forEach((value,i,arr)=>{
+    const currentChanel = showActiveChanel();
+    const index = currentChanel[0].numInArray;
+    startingTime[index].song.forEach((value,i,arr)=>{
         setTimeout(()=>{
             const val = value.code
             allSounds[val].sound.currentTime = 0;
@@ -131,4 +121,11 @@ function playRecordedAudio(){
         },
         value.time)
     })
+}
+
+
+function showActiveChanel(){
+    const resultOfFilter = startingTime.filter(el => el.active);
+    const currentChannel = resultOfFilter[0].numInArray;
+    return resultOfFilter;
 }

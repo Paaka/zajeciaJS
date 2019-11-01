13da835ba03daf373b05d01688dc3264497ced7b
Paaka <Mateuszhartabus@gmail.com>
2019-11-01 10:52:11 +0100

    Now choosing brush works

--- MateuszHartabusLab1/Lab4/scripts/brush.js
+++ MateuszHartabusLab1/Lab4/scripts/brush.js
@@ -1,6 +1,20 @@
-class Brush{
-    constructor(shape){
-        this.shape = shape;
-        console.log(this.shape);
+class Brush {
+    constructor(s){
+        this.shape = s;
+        if(s.shape){
+            this.shape = s.shape;
+        }
+    }
+
+    getBrush(){
+        return this.shape;
+    }
+
+    drawBrush(X,Y, ctx){
+        
+        this.ctx.beginPath();
+        this.ctx.rect(X,Y, 10,10);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
     }
 }
--- MateuszHartabusLab1/Lab4/scripts/draw.js
+++ MateuszHartabusLab1/Lab4/scripts/draw.js
@@ -5,20 +5,33 @@ class Draw{
         this.canvas = document.querySelector(`#${element}`);
         this.ctx = this.canvas.getContext('2d');
         this.color = "black";
+        this.brushShape = 'square';
         this.canvas.addEventListener('touchmove',(e)=> this.paintOnCavas(e));
 
         this.drawBackground()
     }
 
     paintOnCavas(e) {
-        console.log(e)
         const X = e.touches[0].clientX - this.canvas.offsetLeft;
         const Y = e.touches[0].clientY - this.canvas.offsetTop;
+        
+        const brush = new Brush(this.brushShape);
+        this.brushShape = brush.getBrush();
+        
+        if(this.brushShape ==='square'){
+            this.ctx.beginPath();
+            this.ctx.rect(X,Y, 10,10);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+        }else if(this.brushShape ==='circle'){
+            this.ctx.beginPath();
+            this.ctx.arc(X,Y, 5,0,Math.PI*2,false);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+        }
+        
      
-        this.ctx.beginPath();
-        this.ctx.rect(X,Y, 10,10);
-        this.ctx.fillStyle = this.color;
-        this.ctx.fill();
+        
      
      }
 
@@ -32,10 +45,13 @@ class Draw{
     }
 
     chooseColor(e){
-        if(e.target !== e.currentTarget){
+        console.log(e.target)
+        if(e.target !== e.currentTarget){ //btn różny od parenta
             this.color = e.target.value
         }
         e.stopPropagation();
     }
+
+    
 }
 
